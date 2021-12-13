a1575bb508d5743e9d13c631a8479b9ddd13f3a5
威廉伯爵 <techme@live.cn>
2021-12-13 17:16:50 +0800

    Update shangdiyagou.py

--- shangdiyagou.py
+++ shangdiyagou.py
@@ -1,6 +1,6 @@
 from jinja2 import Template
 
 
-s = Template(open('kela.md', encoding='utf8').read()).render()
-with open('readme.md', 'w', encoding='utf8') as f:
+s = Template(open('README.md', encoding='utf8').read()).render()
+with open('README.md', 'w', encoding='utf8') as f:
     f.write(s)
